22f4704cff50094a0dd59edb0058e99fed658c35
lycGit <1148589531@qq.com>
2026-02-08 16:07:20 +0800

    调整有效裁剪区域高度

--- src/js/ImageCropper.js
+++ src/js/ImageCropper.js
@@ -89,7 +89,7 @@ export class ImageCropper {
       z-index: 1000;
     `;
     this.modal.innerHTML = `
-      <div style="position: relative; max-width: 80%; max-height: 80%; width: 100%; height: 100%;">
+      <div style="position: relative; max-width: 85%; max-height: 90%; width: 100%; height: 100%;">
         <div class="modal-header" style="position: absolute; top: -40px; left: 0; right: 0; color: white; display: flex; justify-content: space-between; align-items: center;">
           <h3>裁剪图片</h3>
         </div>
